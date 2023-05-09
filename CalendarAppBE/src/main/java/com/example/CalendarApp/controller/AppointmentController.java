34e5415706abeeaa11d4480bd9a40fda3c084a8d
Stefan Marjanovic <Stefan.Marjanovic@endava.com>
2023-05-09 12:54:33 +0200

    Adding CrossOrigin annotation so BE can communicate with FE

--- CalendarAppBE/src/main/java/com/example/CalendarApp/controller/AppointmentController.java
+++ CalendarAppBE/src/main/java/com/example/CalendarApp/controller/AppointmentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/appointments")
+@CrossOrigin(origins = "*")
 public class AppointmentController {
 
     @Autowired
--- CalendarAppBE/src/main/java/com/example/CalendarApp/controller/InterviewerController.java
+++ CalendarAppBE/src/main/java/com/example/CalendarApp/controller/InterviewerController.java
@@ -13,6 +13,7 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/interviewers")
+@CrossOrigin(origins = "*")
 public class InterviewerController {
 
     @Autowired
