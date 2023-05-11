f834b2618e1c13e6ccaa02f75bccea7ec696d4c0
Tatjana Kunic <tatjana.kunic@endava.com>
2023-05-11 13:06:07 +0200

    Edit styles for Appointment cards

--- calendarappfe/src/components/Appointment.js
+++ calendarappfe/src/components/Appointment.js
@@ -1,23 +1,26 @@
-import { Box, Card, CardContent, Typography, TextField, InputLabel } from "@mui/material";
+import { Card, CardContent, Typography, TextField, Stack } from "@mui/material";
 
 const Appointment = ({appointment}) => {
     return (
         <div>
             <Card sx={{ width: '100%', height:'15%' }}>
-                <CardContent>
+                <CardContent sx={{ padding: '8px 16px', '&:last-child': {paddingBottom: '10px'} }} >
+                    <Stack>
                     <Typography variant="h5" sx={{ mb: 0, float: 'left', fontStyle: 'italic', fontSize: '14pt', border:'none'}} component="div">
-                        <TextField id="outlined-search" label="Candidate Name" type="search" sx={{"& fieldset": { border: 'none' }}}/>
+                        <TextField id="outlined-search" label="Candidate Name" type="search" sx={{"& fieldset": { border: 'none' }}} 
+                        InputLabelProps={{ style: {fontSize: '11pt'} }} inputProps={{style: {paddingBottom: '8px'}}}/>
                     </Typography>
-                    <Box sx={{textAlign: "left"}}>
-                    <Typography sx={{ mt: 0.5, fontSize: '12pt' }} color="text.secondary">
+                    <Stack sx={{textAlign: "left"}}>
+                    <Typography sx={{ mt: 0, fontSize: '11pt' }} color="text.secondary">
                         <label display="inline-block">Experienced interviewer: </label>
                         <input type="text" style={{border:"none"}}/>
                     </Typography>
-                    <Typography color="text.secondary" sx={{ fontSize: '12pt'}}>
+                    <Typography color="text.secondary" sx={{ fontSize: '11pt'}}>
                         <label display="inline-block">Inexperienced interviewer: </label>
                         <input type="text" style={{border:"none"}} />
                     </Typography>
-                    </Box>
+                    </Stack>
+                    </Stack>
                 </CardContent>
             </Card>
         </div>
--- calendarappfe/src/components/Grid.js
+++ calendarappfe/src/components/Grid.js
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Appointment from './Appointment';
-import { Stack } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
