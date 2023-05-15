074c504d9b26ce96171b2f64c830f34e6104ff3e
stefanend <stefan.marjanovic@endava.com>
2023-05-15 17:00:42 +0200

    Adding fetch POST method for adding the Candidate

--- CalendarAppBE/src/main/java/com/example/CalendarApp/domain/model/Candidate.java
+++ CalendarAppBE/src/main/java/com/example/CalendarApp/domain/model/Candidate.java
@@ -16,6 +16,4 @@ public class Candidate {
     private String firstName;
 
     private String lastName;
-
-    private String email;
 }
--- calendarappfe/src/components/Appointment.js
+++ calendarappfe/src/components/Appointment.js
@@ -1,6 +1,39 @@
 import { Card, CardContent, Typography, TextField, Stack } from "@mui/material";
+import { useEffect, useState } from 'react';
+
 
 const Appointment = ({appointment}) => {
+    const [candidate, setCandidate] = useState({
+        firstName: '',
+        lastName: ''
+      });
+
+    const onEnterKey = (keyPress) => {
+        if(keyPress.keyCode === 13) {
+            console.log(keyPress.target.value)
+            const words = keyPress.target.value.split(' ');
+            candidate.firstName = words[0];
+            candidate.lastName = words[1];
+            console.log(candidate)
+            //console.log(appointment.day.toISOString())
+            fetch(`http://localhost:8080/api/appointments/candidate?day=${new Date(appointment.day).toISOString()}`, { 
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain',
+                    'Content-Type':'application/json'},
+                body: JSON.stringify(candidate)
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
+        }
+    }
     return (
         <div>
             <Card sx={{ width: '100%', height:'15%' }}>
@@ -8,7 +41,7 @@ const Appointment = ({appointment}) => {
                     <Stack>
                     <Typography variant="h5" sx={{ mb: 0, float: 'left', fontStyle: 'italic', fontSize: '14pt', border:'none'}} component="div">
                         <TextField id="outlined-search" label="Candidate Name" type="search" sx={{"& fieldset": { border: 'none' }}} 
-                        InputLabelProps={{ style: {fontSize: '11pt'} }} inputProps={{style: {paddingBottom: '8px'}}}/>
+                        InputLabelProps={{ style: {fontSize: '11pt'} }} inputProps={{style: {paddingBottom: '8px'}}} onKeyDown={(e) => onEnterKey(e)}/>
                     </Typography>
                     <Stack sx={{textAlign: "left"}}>
                     <Typography sx={{ mt: 0, fontSize: '11pt' }} color="text.secondary">
