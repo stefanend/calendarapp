a09c893b110d633893a072dd16d8fefdd04c02e9
Tatjana Kunic <tatjana.kunic@endava.com>
2023-05-23 13:14:40 +0200

    Add getAllAppointments integration test

--- CalendarAppBE/src/test/java/com/example/CalendarApp/TestConfig.java
+++ CalendarAppBE/src/test/java/com/example/CalendarApp/TestConfig.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
 public class TestConfig {
 
     protected MockMvc mockMvc;
