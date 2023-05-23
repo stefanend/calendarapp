b9c0984b7903a3a8f6a8b575539278510a11dcda
stefanend <stefan.marjanovic@endava.com>
2023-05-23 12:11:04 +0200

    Adding configuration for Integration tests

---
+++ CalendarAppBE/src/integration-test/java/com/example/CalendarApp/TestConfig.java
@@ -0,0 +1,40 @@
+package com.example.CalendarApp;
+
+import com.example.CalendarApp.domain.repository.AppointmentRepository;
+import com.example.CalendarApp.domain.repository.InterviewerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestConfig {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    public AppointmentRepository appointmentRepository;
+
+    @Autowired
+    public InterviewerRepository interviewerRepository;
+
+    @BeforeAll
+    void setUpRestTemplate() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @AfterEach
+    void clean() {
+        appointmentRepository.deleteAll();
+        interviewerRepository.deleteAll();
+    }
+}
