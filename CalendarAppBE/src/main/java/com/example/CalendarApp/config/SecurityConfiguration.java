c8626cdef3dcab0a9af1ac86e2f95970537c5235
Tatjana Kunic <tatjana.kunic@endava.com>
2023-06-01 16:26:47 +0200

    Permit GET request for appointments without authentication

--- CalendarAppBE/src/main/java/com/example/CalendarApp/config/SecurityConfiguration.java
+++ CalendarAppBE/src/main/java/com/example/CalendarApp/config/SecurityConfiguration.java
@@ -3,12 +3,15 @@ package com.example.CalendarApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -30,17 +33,17 @@ public class SecurityConfiguration {
         return auth;
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.httpBasic()
-                .and()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .logout();
-
-        http.csrf().disable();
-
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(HttpMethod.GET, "/api/appointments").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form.permitAll())
+                .logout((logout) -> logout.permitAll())
+                .httpBasic();
+        return http.build();
     }
 }
